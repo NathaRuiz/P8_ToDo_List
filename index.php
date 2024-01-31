9386e8d1f5b35801195eef175874540e6dc53c1f
NathaRuiz <nruizm98@gmail.com>
2024-01-31 02:08:38 +0100

    create new folders and implements layout

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+//INSERT INTO `tasks` (`id_task`, `title`, `description`, `completed`) VALUES (NULL, 'Crear Tarea', 'Los usuarios PODRÁN agregar nuevas tareas a la lista, proporcionando un título y una descripción.', '0');
+
+?>
---
+++ views/layouts/header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>To Do List</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+        <nav class="navbar navbar-expand navbar-light bg-info shadow-sm mb-5 rounded fw-bold">
+            <div class="nav navbar-nav ">
+                <a class="nav-item nav-link active font-weight-bold" href="#" aria-current="page">Inicio </a>
+                <a class="nav-item nav-link" href="#">Mis Listas</a>
+                <a class="nav-item nav-link" href="#">Cerrar Sesion</a>
+            </div>
+        </nav>
+
+    </header>
+    <main></main>
+    <footer class=" bg-info  text-center mt-5 mb-5  shadow-sm  rounded f">
+
+        <div class="text-center p-3">
+            © 2024 Copyright
+        </div>
+
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
