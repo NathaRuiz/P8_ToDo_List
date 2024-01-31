bc8133b82cfec28132d1d0033d8f716b595afa9a
NathaRuiz <nruizm98@gmail.com>
2024-01-31 02:17:23 +0100

    implement and separate header and footer

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 //INSERT INTO `tasks` (`id_task`, `title`, `description`, `completed`) VALUES (NULL, 'Crear Tarea', 'Los usuarios PODRÁN agregar nuevas tareas a la lista, proporcionando un título y una descripción.', '0');
-
+include("./views/layouts/header.php");
 ?>
+
+Bienvenido
+
+<?php include("./views/layouts/footer.php"); ?>
