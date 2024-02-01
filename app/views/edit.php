f60fe0e337a03df9d37af96c5d33397f7ad11eb4
NathaRuiz <nruizm98@gmail.com>
2024-02-01 14:47:42 +0100

    add title in index.php

---
+++ app/views/edit.php
@@ -0,0 +1,35 @@
+<?php include_once __DIR__ . '/layouts/header.php' ?>
+
+
+<section class="mt-5 py-2 px-5">
+    <h2 class="pt-4">Crear Nueva Tarea</h2>
+    <form action="index.php?action=store" method="POST">
+        <div class="mb-3">
+            <label for="title" class="form-label">Título:</label>
+            <input type="text" class="form-control" name="title" required>
+        </div>
+
+        <div class="mb-3">
+            <label for="task_description" class="form-label">Descripción:</label>
+            <textarea class="form-control" name="task_description"></textarea>
+        </div>
+
+        <div class="mb-3">
+            <label for="task_state" class="form-label">Estado:</label>
+            <select class="form-select" id="inputGroupSelect01" name="task_state">
+                <option selected>Escoge...</option>
+                <option value="Por Hacer">Por Hacer</option>
+                <option value="En Progreso">En Progreso</option>
+                <option value="Finalizado">Finalizado</option>
+            </select>
+        </div>
+
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Guardar Tarea</button>
+            <button type="button" class="btn btn-secondary" onclick="location.href='index.php?action=index'">Cancelar</button>
+        </div>
+    </form>
+</section>
+
+
+<?php include_once __DIR__ . '/layouts/footer.php'; ?>
--- app/views/index.php
+++ app/views/index.php
@@ -1,8 +1,9 @@
 <?php include_once __DIR__ . '/layouts/header.php'; ?>
 
 <section class="mt-5 mx-3">
+<h2 class="pt-4">Mi lista de Tareas</h2>
 <form  action="index.php?action=create" method="POST">
-<button type="submit" class="btn btn-primary mt-5">Crear Tarea</button>
+<button type="submit" class="btn btn-primary mt-2 ">Crear Tarea</button>
 </form>
     <div class="table-responsive">
         <?php if (!empty($tasks)): ?>
